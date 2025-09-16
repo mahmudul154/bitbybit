86deb88c2d092ec668ec4d0cbd7b5a40160ac7a0
mahmudul154 <aakashh060@gmail.com>
2025-09-16 16:07:07 -0600

    feat: Remove API keys from source control and move to .env.local

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,25 +1,18 @@
 // src/lib/firebase.ts
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-// Your Firebase configuration object
 const firebaseConfig = {
-  apiKey:"AIzaSyDBU0Jr5QPFSlx8GL5_P70w_dKI3Qcisyo",
-  authDomain: "bitbybit-125ee.firebaseapp.com",
-  projectId: "bitbybit-125ee",
-  storageBucket: "bitbybit-125ee.firebasestorage.app",
-  messagingSenderId: "936483093657",
-  appId:  "1:936483093657:web:78a483119e1dfd31f0cc08",
-  measurementId: "G-QNGM270CQC"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-
-// --- THIS IS THE FIX ---
-// This pattern prevents re-initializing the app on hot reloads in development.
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// Initialize and export Firebase services
 export const auth = getAuth(app);
 export const storage = getStorage(app);
